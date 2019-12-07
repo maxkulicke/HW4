e7ed8df4022a389e92f657a2bdc7dde6a30018ff
Max Kulicke <mkulicke@gmail.com>
2019-12-07 14:59:30 -0500

    added some imports (jQuery, bootstrap etc) and created function skeletons in script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
 
+  function quizRunner() {
+
+  }
+
+  function questionThrower() {
+
+  }
+
+  function display() {
+
+  }
+
+  function click() {
+
+  }
+
+  function checkAnswer() {
+
+  }
+
+  function decrement() {
+
+  }
+
+  function scoreboard() {
+    
+  }
 
 
-  
+
 }
